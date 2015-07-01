78acec2411fcc7823e9c16e2501fbd3fbf8528f8
Chilipp <p.sommer@posteo.de>
2015-07-01 08:21:12 +0200

    Added init for data directory

---
+++ data/__init__.py
@@ -0,0 +1,7 @@
+"""Data module in nc2map python module
+
+Contained:
+  - boxes: dictionary containing longitude-latitude boundary boxes for all
+      countries and continents covered by the Vmap0 dataset
+  - polygon shapefiles for the lineshapes formatoption keyword
+"""
